5784d7f4c2c1e76d2147cac2d2e10e3b71eb37c8
Myungji Choi <choimyungji@gmail.com>
2022-05-18 23:33:40 +0900

    add darkmode

--- js/main.js
+++ js/main.js
@@ -39,3 +39,50 @@ function menuClick() {
 }
 
 menuClick();
+
+function detectColorScheme() {
+	var theme="light";    
+	 //default to light
+       
+	//local storage is used to override OS theme settings
+	if(localStorage.getItem("theme")){
+	    if(localStorage.getItem("theme") == "dark"){
+		var theme = "dark";
+	    }
+	} else if(!window.matchMedia) {
+	    //matchMedia method not supported
+	    return false;
+	} else if(window.matchMedia("(prefers-color-scheme: dark)").matches) {
+	    //OS theme setting detected as dark
+	    var theme = "dark";
+	}
+       
+	//dark theme preferred, set document with a `data-theme` attribute
+	if (theme=="dark") {
+	    document.documentElement.setAttribute("data-theme", "dark");
+	}
+	}
+	detectColorScheme();
+	 //identify the toggle switch HTML element
+	     const themeSwitch = document.getElementById('hm-theme-switch-toggle');
+       
+	//function that changes the theme, and sets a localStorage variable to track the theme between page loads
+	function switchTheme(e) {
+	if (e.target.checked) {
+	    localStorage.setItem('theme', 'dark');
+	    document.documentElement.setAttribute('data-theme', 'dark');
+	    themeSwitch.checked = true;
+	} else {
+	    localStorage.setItem('theme', 'light');
+	    document.documentElement.setAttribute('data-theme', 'light');
+	    themeSwitch.checked = false;
+	  }    
+	 }
+       
+	  //listener for changing themes
+	//     themeSwitch.addEventListener('change', switchTheme, false);
+       
+       //pre-check the dark-theme checkbox if dark-theme is set
+	//    if (document.documentElement.getAttribute("data-theme") == "dark"){
+	//    themeSwitch.checked = true;
+	// }
